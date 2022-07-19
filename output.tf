b882f71427d8257fb643d84161c9b732a238c7ea
jveraduran <51175225+jveraduran@users.noreply.github.com>
2022-07-19 14:55:56 -0400

    feat: Add linter

--- output.tf
+++ output.tf
@@ -1,17 +1,21 @@
 output "kubeconfig" {
   value = huaweicloud_cce_cluster.mycce.kube_config_raw
+  description = "kubeconfig"
 }
 
 output "lb_public_ip" {
   value = huaweicloud_vpc_eip.eip-lb.address
+  description = "Public IP"
 }
 
 output "lb_id" {
   value = huaweicloud_elb_loadbalancer.loadbalancer.id
+  description = "LoadBalancer ID"
 }
 
 output "lb_subnet_id" {
   value = huaweicloud_elb_loadbalancer.loadbalancer.ipv4_subnet_id
+  description = "LoadBalancer Subnet ID"
 }
 
 
