2ff327c21e5961f800db8f3ed6843560fd6031d8
jveraduran <51175225+jveraduran@users.noreply.github.com>
2022-07-21 21:49:10 -0400

    feat: Enable LoadBalancer Module

--- main.tf
+++ main.tf
@@ -15,8 +15,6 @@ resource "huaweicloud_compute_keypair" "cce_node" {
 }
 
 data "huaweicloud_availability_zones" "myaz" {}
-
-
 resource "huaweicloud_cce_node" "mynode" {
   count             = var.node_count
   cluster_id        = huaweicloud_cce_cluster.mycce.id
@@ -37,43 +35,15 @@ resource "huaweicloud_cce_node" "mynode" {
   tags = var.default_tags
 }
 
-data "huaweicloud_elb_flavors" "flavors" {
-  count = var.enable_elb_loadbalancer ? 1 : 0
-  
-  type            = "L4"
-  max_connections = var.lb_max_connections
-}
-resource "huaweicloud_elb_loadbalancer" "loadbalancer" {
-  count = var.enable_elb_loadbalancer ? 1 : 0
+module "loadbalancer" {
+  count  = var.enable_elb_loadbalancer == true ? 1 : 0
+  source = "./modules/load-balancer"
 
-  name              = "${var.cluster_name}-elb"
-  description       = "Dedicated Loadbalancer for ${var.cluster_name}"
-  cross_vpc_backend = true
-
-  vpc_id = var.vpc_id
-
-  l4_flavor_id = data.huaweicloud_elb_flavors.flavors.ids[0]
-
-  availability_zone = [
-    data.huaweicloud_availability_zones.myaz.names[0],
-    data.huaweicloud_availability_zones.myaz.names[1],
-  ]
-
-  ipv4_eip_id = huaweicloud_vpc_eip.eip_lb.id
-
-  tags = var.default_tags
-}
-
-resource "huaweicloud_vpc_eip" "eip_lb" {
-  count = var.enable_elb_loadbalancer ? 1 : 0
-  publicip {
-    type = "5_bgp"
-  }
-  bandwidth {
-    name        = "${var.cluster_name}-lb-eip-bw"
-    size        = var.lb_bandwidth_size
-    share_type  = var.lb_share_type
-    charge_mode = var.lb_charge_mode
-  }
-  tags = var.default_tags
+  cluster_name       = "${var.cluster_name}-cluster"
+  vpc_id             = var.vpc_id
+  default_tags       = var.default_tags
+  lb_bandwidth_size  = var.lb_bandwidth_size
+  lb_share_type      = var.lb_share_type
+  lb_charge_mode     = var.lb_charge_mode
+  lb_max_connections = var.lb_max_connections
 }
---
+++ modules/load-balancer/outputs.tf
@@ -0,0 +1,12 @@
+output "lb_public_ip" {
+  value       = huaweicloud_vpc_eip.eip_lb.address
+  description = "Public IP"
+}
+output "lb_id" {
+  value       = huaweicloud_elb_loadbalancer.loadbalancer.id
+  description = "LoadBalancer ID"
+}
+output "lb_subnet_id" {
+  value       = huaweicloud_elb_loadbalancer.loadbalancer.ipv4_subnet_id
+  description = "LoadBalancer Subnet ID"
+}
